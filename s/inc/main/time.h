759be23811bd9cfdc70f40c24acc141b46b2477d
bc <costin.botescu@gmail.com>
2025-06-18 17:49:48 +0300

    some win tests, and since stat, also at arm7

--- s/inc/main/time.h
+++ s/inc/main/time.h
@@ -1,5 +1,20 @@
 
-typedef long int time_t;
+#undef platform_is_known
+#ifdef SIZEOF_INT_P
+#	if SIZEOF_INT_P >= 8
+#		ifdef SIZEOF_LONG
+#			if SIZEOF_LONG == 4
+#				define platform_is_known 1
+				typedef long long time_t; //win 64 gcc
+#			endif
+#		endif
+#	endif
+#endif
+#ifndef platform_is_known
+	typedef long int time_t;
+#else
+#	undef platform_is_known
+#endif
 
 #ifdef __cplusplus
 extern "C" {
--- s/inc/split/string.h
+++ s/inc/split/string.h
@@ -6,7 +6,9 @@ extern "C" {
 #endif
 
 void*memchr(void*,int,size_t);
-//void *memmem(const void*,size_t,const void*,size_t);//this is provided in our split.c
+#ifdef HAVE_MEMMEM  //hard to happen
+void *memmem(const void*,size_t,const void*,size_t);//this is provided in our split.c
+#endif
 
 #ifdef __cplusplus
 }
